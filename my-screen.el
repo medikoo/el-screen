b36e6c6e40698bf75fc3220f3f15a7c082ff82b5
Mariusz Nowak <mariusz@medikoo.com>
2010-01-28 17:14:38 +0100

    Renamed my-tasks into my-screen

--- my-screen.el
+++ my-screen.el
@@ -1,7 +1,7 @@
-;; my-tasks.el --- switchable screen configurations
+;; my-screen.el --- switchable screen configurations
 
-;; Author:	Mariusz Nowak <mariusz+emacs.my-tasks@medikoo.com>
-;; Copyright (C) 2010 Mariusz Nowak <mariusz+emacs.my-tasks@medikoo.com>
+;; Author:	Mariusz Nowak <mariusz+emacs.my-screen@medikoo.com>
+;; Copyright (C) 2010 Mariusz Nowak <mariusz+emacs.my-screen@medikoo.com>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -21,72 +21,72 @@
 (require 'my/frame)
 (require 'my/file)
 
-(defvar my-tasks-dir
+(defvar my-screen-dir
 	(concat (getenv "HOME") "/.emacs.d/.my/")
-	"Directory to save tasks in.")
+	"Directory to save screens in.")
 
-(defvar my-tasks-file-extension
-	".mytask"
-	"File extension for frame bookmarks.")
+(defvar my-screen-file-extension
+	".myscreen"
+	"File extension for screen configurations.")
 
-(defvar my-tasks-current
+(defvar my-screen-current
 	""
-	"Name of current frame configuration.")
+	"Name of current screen configuration.")
 
-(defvar my-tasks-current-name-file
-	".mytask-current"
-	"Filname of file that holds current frame configuration name.")
+(defvar my-screen-current-name-file
+	".myscreen-current"
+	"Filname of file that holds current screen configuration name.")
 
-(defun my-tasks-set-name (name)
+(defun my-screen-set-name (name)
 	(my-file-write
-		(concat my-tasks-dir my-tasks-current-name-file) name)
-	(setq my-tasks-current name))
+		(concat my-screen-dir my-screen-current-name-file) name)
+	(setq my-screen-current name))
 
-(defun my-tasks-save ()
+(defun my-screen-save ()
 	(my-file-write
-		(concat my-tasks-dir
-			my-tasks-current my-tasks-file-extension)
+		(concat my-screen-dir
+			my-screen-current my-screen-file-extension)
 		(prin1-to-string (my-frame-serialize)))
-	(message "Task saved: %S" my-tasks-current))
+	(message "Screen saved: %S" my-screen-current))
 
-(defun my-tasks-save-as (name)
-	(my-tasks-set-name name)
-	(my-tasks-save))
+(defun my-screen-save-as (name)
+	(my-screen-set-name name)
+	(my-screen-save))
 
-(defun my-tasks-new (name)
+(defun my-screen-new (name)
 	(delete-other-windows)
 	(set-window-buffer (selected-window) "*scratch*")
-	(my-tasks-new-configure)
-	(my-tasks-save-as name))
+	(my-screen-new-configure)
+	(my-screen-save-as name))
 
-(defun my-tasks-new-configure ())
+(defun my-screen-new-configure ())
 
-(defun my-tasks-load (name)
-	(let ((data (my-file-read (concat my-tasks-dir
-						name my-tasks-file-extension) t)))
+(defun my-screen-load (name)
+	(let ((data (my-file-read (concat my-screen-dir
+						name my-screen-file-extension) t)))
 		(if data
 			(progn
 				(my-frame-unserialize (read data))
-				(my-tasks-set-name name))
-			(my-tasks-new name))
-		(message "Task loaded: %S" name)))
+				(my-screen-set-name name))
+			(my-screen-new name))
+		(message "Screen loaded: %S" name)))
 
-(defun my-tasks-switch (name)
+(defun my-screen-switch (name)
 	(interactive "sName: ")
-	(my-tasks-save)
-	(my-tasks-load name))
+	(my-screen-save)
+	(my-screen-load name))
 
-(defun my-tasks-init ()
-	(my-tasks-load (or
-			(my-file-read (concat my-tasks-dir my-tasks-current-name-file) t)
-			my-tasks-current))
-	(add-hook 'auto-save-hook 'my-tasks-save)
-	(add-hook 'kill-emacs-hook 'my-tasks-save))
+(defun my-screen-init ()
+	(my-screen-load (or
+			(my-file-read (concat my-screen-dir my-screen-current-name-file) t)
+			my-screen-current))
+	(add-hook 'auto-save-hook 'my-screen-save)
+	(add-hook 'kill-emacs-hook 'my-screen-save))
 
-(defun my-tasks-print-current ()
+(defun my-screen-print-current ()
 	(interactive)
-	(message my-tasks-current))
+	(message my-screen-current))
 
-(provide 'my-tasks/my-tasks)
+(provide 'my-screen/my-screen)
 
-(my-tasks-init)
+(my-screen-init)
