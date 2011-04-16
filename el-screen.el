c884c0d5a5947a5d88a44576437923dd03772ebd
Mariusz Nowak <medyk@medikoo.com>
2011-04-16 20:29:31 +0200

    Copyright info

--- el-screen.el
+++ el-screen.el
@@ -1,7 +1,7 @@
 ;; el-screen.el --- Window configurations manager for Emacs
 
-;; Author:	Mariusz Nowak <mariusz+el-screen@medikoo.com>
-;; Copyright (C) 2010, 2011 Mariusz Nowak <mariusz+el-screen@medikoo.com>
+;; Author:	Mariusz Nowak <medikoo+el-screen@medikoo.com>
+;; Copyright (C) 2010, 2011 Mariusz Nowak <medikoo+el-screen@medikoo.com>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -11,7 +11,7 @@
 ;; This program is distributed in the hope that it will be
 ;; useful, but WITHOUT ANY WARRANTY; without even the implied
 ;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-;; PURPOSE.	 See the GNU General Public License for more details.
+;; PURPOSE. See the GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public
 ;; License along with this program; if not, see <http://www.gnu.org/licenses/>.
